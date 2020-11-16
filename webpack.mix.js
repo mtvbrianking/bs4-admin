3bcc071c46fba4242b0e8cb661462761f9bfcfb6
mtvbrianking <mtvbrianking@gmail.com>
2020-11-16 14:40:46 +0300

    Improve dir structure.

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,10 +2,10 @@ const mix = require('laravel-mix');
 
 mix.sourceMaps();
 
-mix.js('resources/js/app.js', 'assets/js');
-mix.js('resources/js/layout.js', 'assets/js');
+mix.js('resources/js/app.js', 'public/assets/js');
+mix.js('resources/js/layout.js', 'public/assets/js');
 
-mix.sass('resources/sass/app.scss', 'assets/css');
+mix.sass('resources/sass/app.scss', 'public/assets/css');
 
-mix.copy('node_modules/boxicons/css/boxicons.min.css', 'assets/vendor/boxicons/css/boxicons.min.css');
-mix.copyDirectory('node_modules/boxicons/fonts', 'assets/vendor/boxicons/fonts');
+mix.copy('node_modules/boxicons/css/boxicons.min.css', 'public/assets/vendor/boxicons/css/boxicons.min.css');
+mix.copyDirectory('node_modules/boxicons/fonts', 'public/assets/vendor/boxicons/fonts');
